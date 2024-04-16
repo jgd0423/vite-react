7b5fa7838fe88f9e5fe6d60bad34542c17f97823
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-04-16 16:40:15 +0900

    69. Next 프로젝트 선행 및 심화 개념

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-const entryPoint = document.getElementById('root');
-ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
