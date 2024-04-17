f05960c46b0fc23e1ece0eeb9946b074d60c8686
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-04-17 14:24:13 +0900

    87. 조건적 버튼 비활성화

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -14,18 +14,6 @@ export default function GameBoard({ onSelectSquare, turns }) {
     gameBoard[row][col] = player;
   });
 
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  //
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard(prevGameBoard => {
-  //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-  //
-  //   onSelectSquare();
-  // }
-
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
@@ -33,7 +21,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={!!playerSymbol}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
