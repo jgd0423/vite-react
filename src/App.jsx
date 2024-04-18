6cfa7cd871b826825d2157945ff473aec5df8dbf
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-04-18 14:47:18 +0900

    104. 모듈 소개 & 프로젝트 시작하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,13 @@
+import AuthInputs from './components/AuthInputs.jsx';
 import Header from './components/Header.jsx';
-import { calculateInvestmentResults } from './util/investment.js';
-import UserInput from './components/UserInput.jsx';
-import { useState } from 'react';
-import Results from './components/Results.jsx';
 
-const initialInvestmentInfo = {
-  initialInvestment: 10000,
-  annualInvestment: 1200,
-  expectedReturn: 6,
-  duration: 10,
-};
-
-function App() {
-  const [investmentInfo, setInvestmentInfo] = useState({ ...initialInvestmentInfo });
-
-  const isInvestmentInfoValid = investmentInfo.duration >= 1;
-
-  function handleInvestmentInfoChange(inputIdentifier, newValue) {
-    setInvestmentInfo(prevInfo => {
-      return {
-        ...prevInfo,
-        [inputIdentifier]: parseInt(newValue),
-      };
-    });
-  }
-
-  const investmentResults = calculateInvestmentResults(investmentInfo);
-
+export default function App() {
   return (
     <>
       <Header />
-      <UserInput investmentInfo={investmentInfo} onInvestmentInfoChange={handleInvestmentInfoChange} />
-      {!isInvestmentInfoValid && <p className="center">Please enter a duration greater than zero.</p>}
-      {isInvestmentInfoValid && <Results investmentResults={investmentResults} />}
+      <main>
+        <AuthInputs />
+      </main>
     </>
   );
 }
-
-export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import logo from '../assets/investment-calculator-logo.png';
+import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <div id="header">
-      <img src={logo} alt="logo" />
-      <h1>Investment Calculator</h1>
-    </div>
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p>A community of artists and art-lovers.</p>
+    </header>
   );
 }
