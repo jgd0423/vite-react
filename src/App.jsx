ee0677333cb293f4e3d1bd6c92fcbe6e0f349c4b
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-04-24 09:56:36 +0900

    153. Refs(참조)와 전달된 Refs(참조)로 사용자 입력 받아오기

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,29 @@ function App() {
   function handleStartAddProject() {
     setProjectsState(prevState => {
       return {
+        ...prevState,
         selectedProjectId: null,
       };
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = { ...projectData, id: Math.random() };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
