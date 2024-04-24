22bba8207b2ee8ddfa89a5c1c56e473f58e80cb0
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-04-24 15:50:23 +0900

    161. 태스크 지우기 & 사소한 버그 고치기

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,14 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter(task => task.id !== id),
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
