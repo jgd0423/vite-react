3a8b2df9d1c0679cacb7bd0bdec14ccb69d81ada
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-04-26 10:23:41 +0900

    168. 컨텍스트와 State(상태) 연결하기

--- src/App.jsx
+++ src/App.jsx
@@ -63,13 +63,18 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={ctxValue}>
       <Header cart={shoppingCart} onUpdateCartItemQuantity={handleUpdateCartItemQuantity} />
       <Shop>
         {DUMMY_PRODUCTS.map(product => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </Shop>
