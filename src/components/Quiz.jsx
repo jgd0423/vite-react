d6ca594a6109ecbdf229437682b786d6670e7d9e
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-05-09 14:27:20 +0900

    193. 값 파생시키기, 질문 출력 및 답변 등록

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,30 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers(prevAnswers => [selectedAnswer, ...prevAnswers]);
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS.at(activeQuestionIndex).text}</h2>
+        <ul id="answers">
+          {QUESTIONS.at(activeQuestionIndex).answers.map(answer => {
+            return (
+              <li key={answer} className="answer">
+                <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
