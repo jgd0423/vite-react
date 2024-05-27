358ae29d6be8c68898330d5f932e1d4c3cb9e2b5
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-05-27 13:20:49 +0900

    204. 리액트의 컴포넌트 트리 생성 / 리액트가 시스템 뒷편에서 동작하는 방식

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-import logoImg from '../assets/quiz-logo.png';
+import { log } from '../log.js';
+import logoImg from '../assets/logo.png';
 
 export default function Header() {
+  log('<Header /> rendered', 1);
+
   return (
-    <header>
-      <img src={logoImg} alt="Quiz logo" />
-      <h1>ReactQuiz</h1>
+    <header id="main-header">
+      <img src={logoImg} alt="Magnifying glass analyzing a document" />
+      <h1>React - Behind The Scenes</h1>
     </header>
   );
 }
