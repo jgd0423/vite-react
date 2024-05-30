f085c693514d7c2d96361ee4b7ca638655c73953
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-05-30 17:53:56 +0900

    236. 코드 추출 및 코드 구조 개선

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { useEffect, useState } from 'react';
 import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -13,19 +14,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch places.');
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition(position => {
-          const sortedPlaces = sortPlacesByDistance(
-            resData.places,
-            position.coords.latitude,
-            position.coords.longitude,
-          );
+          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
           setAvailablePlaces(sortedPlaces);
           setIsFetching(false);
         });
