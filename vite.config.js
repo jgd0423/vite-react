db704f4df735794c007c70c6130ee0864a0a722b
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-05-30 15:09:47 +0900

    218. 퍼스트 클래스 기반 컴포넌트 추가하기

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import MillionLint from '@million/lint';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), MillionLint.vite()],
+  plugins: [react()],
 });
