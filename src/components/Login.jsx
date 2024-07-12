6609b456948363034349011a218e7c639851d063
Jeong Geumdam <jgd0423@neighbor21.co.kr>
2024-07-12 14:43:45 +0900

    251. State(상태) & 일반 Handler로 사용자 입력 수집 및 관리

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log('Submitted!');
+    console.table(enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
   }
 
   return (
@@ -11,12 +25,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={event => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={event => handleInputChange('password', event.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
