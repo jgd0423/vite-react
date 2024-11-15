330828ad09aed99e7206a5a3561150353b84b20a
Jeong Geumdam <>
2024-11-15 15:30:50 +0900

    301. 다중 슬라이스 작업하기

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,10 @@
+// immer.js가 내장되어있음
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
-
-// immer.js가 내장되어있음
+const initialCounterState = { counter: 0, showCounter: true };
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment: state => {
       state.counter++;
@@ -22,10 +21,25 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = { isAuthenticated: false };
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login: state => {
+      state.isAuthenticated = true;
+    },
+    logout: state => {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
