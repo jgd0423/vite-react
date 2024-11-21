25ea8c30038005e08c1288e05b82bd3cef4f53d4
Jeong Geumdam <>
2024-11-21 17:09:06 +0900

    326. 라우트를 정의하는 다른 방법들

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
-import Products from './pages/Products';
+import ProductsPage from './pages/Products';
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" component={HomePage} />
+//     <Route path="/products" component={ProductsPage} />
+//   </Route>,
+// );
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
-  { path: '/products', element: <Products /> },
+  { path: '/products', element: <ProductsPage /> },
 ]);
+
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
