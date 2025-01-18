a5c34905f306500204ef1e4ff342cb80a552ee79
Jeong Geumdam <>
2025-01-18 16:06:15 +0900

    391. 프로젝트 설정 및 개요

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error", // Hooks 규칙 강제
+      "react-hooks/exhaustive-deps": "warn", // useEffect 의존성 배열 체크
+      "react/react-in-jsx-scope": "off",
+    },
+  }
+];
