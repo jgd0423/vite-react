a78615b446f426d21f5c65209925f9d5a222cc6f
Jeong Geumdam <>
2025-01-26 18:24:57 +0900

    415. 변형을 사용하여 데이터 변경

--- src/util/http.js
+++ src/util/http.js
@@ -6,7 +6,7 @@ export async function fetchEvents({ signal, searchTerm }) {
     url += '?search=' + searchTerm;
   }
 
-  const response = await fetch(url, { signal });
+  const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the events');
@@ -18,4 +18,25 @@ export async function fetchEvents({ signal, searchTerm }) {
   const { events } = await response.json();
 
   return events;
+}
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while creating the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
 }
