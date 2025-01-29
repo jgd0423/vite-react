ef23aacba8919106f7427c7246e9466208d88a9f
Jeong Geumdam <>
2025-01-29 14:31:47 +0900

    417. 변형 성공 시 동작 및 쿼리 무효화

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './util/http.js';
 
 const router = createBrowserRouter([
   {
@@ -35,8 +36,6 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const queryClient = new QueryClient();
-
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
