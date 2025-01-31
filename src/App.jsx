7cccad780dd96c758352709bc34f34b3f6d3d195
Jeong Geumdam <>
2025-01-31 21:30:11 +0900

    426. 리액트 쿼리와 리액트 라우터

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
-import EditEvent from './components/Events/EditEvent.jsx';
-import { QueryClientProvider } from '@tanstack/react-query';
+import EditEvent, { loader as editEventLoader, action as editEventAction } from './components/Events/EditEvent.jsx';
 import { queryClient } from './util/http.js';
 
 const router = createBrowserRouter([
@@ -15,7 +15,6 @@ const router = createBrowserRouter([
   {
     path: '/events',
     element: <Events />,
-
     children: [
       {
         path: '/events/new',
@@ -30,6 +29,8 @@ const router = createBrowserRouter([
       {
         path: '/events/:id/edit',
         element: <EditEvent />,
+        loader: editEventLoader,
+        action: editEventAction,
       },
     ],
   },
